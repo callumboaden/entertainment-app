f4168093e4a0cda1846768dc605a2f0f34c45f2c
callumboaden <boaden.callum@gmail.com>
2022-10-24 17:35:53 +0700

    removed unescaped entities

--- pages/account/login.js
+++ pages/account/login.js
@@ -30,7 +30,7 @@ export default function LoginPage() {
 
   useEffect(() => {
     loadUser();
-  }, []);
+  });
 
   const onSubmit = async (e) => {
     try {
@@ -73,7 +73,8 @@ export default function LoginPage() {
           <input type="submit" value="Login" />
         </form>
         <p>
-          Don't have an account? <Link href="/account/register">Register</Link>
+          Don&apos;t have an account?
+          <Link href="/account/register">Register</Link>
         </p>
       </div>
     </Layout>
